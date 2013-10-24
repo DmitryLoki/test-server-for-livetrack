f766ff7f206a2569c417f113ac91bc653519f92d
Dmitry Korolev <dmitry@globalinfo.ru>
2013-10-24 13:29:25 +0400

    First final version of test server

--- server.rb
+++ server.rb
@@ -3,49 +3,64 @@
 require 'socket' 
 require 'rubygems'
 require 'json'
-
+require 'mysql'
+  
 server = TCPServer.open("localhost", 8001) # открываем сервак на 8001 порту локалхоста
+mysqlserv = Mysql::new("localhost", "root", "root", "snowdb") # локальное подключение на вагранте
+
+=begin
+myres = mysqlserv.query("SELECT * FROM `users`")
+data = myres.fetch_hash
+p data['username']
+=end
 
 loop do
-	client = server.accept
-	line = client.gets
-	
-	# если на вход приходит запрос на координаты юзеров,
-	# то выдаем в ответ JSON с координатами
-	if line[0..0] == "?"
-		array_of_users = line[1...-1].split(",")
+	Thread.fork(server.accept) do |client|
 
-		client.print "Return track for users:\n" 
-		array_of_users.each do |user|
-			# вместо этого надо вставить код который берет из базы
-			user_data = {'udid' => user, 'lat' => rand(55-10) + rand(), 'lon' => rand(55-10) + rand(), 'alt' => rand(8700-100)}
-			# =================================================
-			client.print user_data.to_json # перегнали хеш в json и отправили клиенту
-			client.print "\n"
+		line = client.gets
+
+		# если на вход приходит запрос на координаты юзеров,
+		# то выдаем в ответ JSON с координатами
+		
+		if line[0..0] == "?"
+			array_of_users = line[1...-1].split(",")
+
+			array_of_users.each do |user|
+				# берем из базы последнюю точку для запрашиваемого юзера или юзеров, и возвращаем json
+				query = "SELECT * FROM `trackpoints` WHERE `uid` = '#{user}' ORDER BY `timestamp` DESC"
+				user_data = mysqlserv.query(query).fetch_hash
+				# =================================================
+				client.print user_data.to_json # перегнали хеш в json и отправили клиенту
+				client.print "\n"
+				
+			end
+			
+		# ======================================
+		else
+		# если нет то парсим строку как JSON (надо добавить проверку на валидный JSON)
+		#  и обрабатываем данные
+
+			result = JSON.parse(line)
+			udid = result["udid"]
+			lat = result["lat"]
+			lon = result["lon"]
+			alt = result["alt"]
+			time = result["time"]
+
+			# сохраняем в базу координаты
+			query = "INSERT INTO `trackpoints` VALUES ('#{udid}', '#{lat}', '#{lon}', '#{alt}', '#{time}')"
+
+			mysqlserv.query(query)
+
+			logfile = File.open("/tmp/snowlog", 'a')
+			logfile.puts(Time.now.to_i.to_s + " - #{line}")
+			logfile.close			
+			# =================================
+
+			
 		end
 		client.close
-	# ======================================
-	else
-	# если нет то парсим строку как JSON (надо добавить проверку на валидный JSON)
-	# и обрабатываем данные
+		# =======================================
 
-		result = JSON.parse(line)
-		
-		udid = result["udid"]
-		lat = result["lat"]
-		lon = result["lon"]
-		alt = result["alt"]
-
-		# ответ о том что координаты дошли
-		# вместо кода ниже нужно добавить сохранение данных в бд
-		answer = "Saved point for user " + udid.to_s + "\n" +
-			"with latitude = " + lat.to_s + " and longitude = " + lon.to_s + ".\n" +
-			"Altitude is " + alt.to_s + " meters.\n"
-		client.print answer
-		# =================================
-
-		client.close
 	end
-	# =======================================
-
 end
